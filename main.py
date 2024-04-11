3dd6e25fde18ff6d1058e30a808d1a4980d4841c
Pavel Bologan <p.bologan@gmail.com>
2024-04-11 18:23:05 +0700

    delete start code

--- main.py
+++ main.py
@@ -23,6 +23,3 @@ app.add_middleware(
 @app.get("/chart")
 async def chart(x_values: List[float] = Query(None), y_values: List[float] = Query(None), step: float = Query(None), precision: int = Query(None)):
     return get_chart_data(x=x_values, y=y_values, step=step, precision=precision)
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
